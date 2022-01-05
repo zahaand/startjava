14e8e332b172587c503bb06f37c6c821d9a06c5e
Andrey Zakharenkov <zahaand@gmail.com>
2022-01-06 00:46:27 +0300

    modified queries.sql

--- src/com/startjava/lesson_5/queries.sql
+++ src/com/startjava/lesson_5/queries.sql
@@ -1,10 +1,11 @@
 SELECT * FROM jaegers;
-SELECT * FROM jaegers WHERE status = 'true';
+SELECT * FROM jaegers WHERE status = 'false';
 SELECT * FROM jaegers WHERE mark = 'Mark-1' AND 'Mark-4';
 SELECT * FROM jaegers ORDER BY mark DESC;
 SELECT MAX(launch) FROM jaegers;
 SELECT MAX(kaijuKill) FROM jaegers;
 SELECT MIN(kaijuKill) FROM jaegers;
 SELECT AVG(weight) FROM jaegers;
-UPDATE jaegers SET kaijuKill = 'kaijuKill' + 1 WHERE status = 'true';
+UPDATE jaegers SET kaijuKill = kaijuKill + 1 WHERE status = 'false';
+SELECT * FROM jaegers;
 DELETE FROM jaegers WHERE status = 'false';
