657bbe8bde3be2fe98d5fe865265117454eb7644
Andrey Zakharenkov <zahaand@gmail.com>
2022-01-09 01:49:16 +0300

    format code create_db, queries

--- src/com/startjava/lesson_5/create_db.sql
+++ src/com/startjava/lesson_5/create_db.sql
@@ -3,12 +3,12 @@ CREATE DATABASE jaegers;
 \connect jaegers;
 
 CREATE TABLE jaegers (
-    id SERIAL PRIMARY KEY, 
-    modelName TEXT, 
-    mark TEXT, 
-    height INTEGER, 
-    weight DOUBLE PRECISION, 
-    status BOOLEAN, 
-    origin TEXT, 
-    launch INTEGER, 
-    kaijuKill INTEGER);
+    id          SERIAL PRIMARY KEY, 
+    modelName   TEXT, 
+    mark        TEXT, 
+    height      INTEGER, 
+    weight      REAL, 
+    status      BOOLEAN, 
+    origin      TEXT, 
+    launch      INTEGER, 
+    kaijuKill   INTEGER);
--- src/com/startjava/lesson_5/queries.sql
+++ src/com/startjava/lesson_5/queries.sql
@@ -1,10 +1,39 @@
-SELECT * FROM jaegers;
-SELECT * FROM jaegers WHERE status = 'true';
-SELECT * FROM jaegers WHERE mark = 'Mark-1' OR mark = 'Mark-4';
-SELECT * FROM jaegers ORDER BY mark DESC;
-SELECT * FROM jaegers WHERE launch = (SELECT MIN(launch) FROM jaegers) LIMIT 1;
-SELECT * FROM jaegers WHERE kaijuKill = (SELECT MAX(kaijuKill) FROM jaegers);
-SELECT * FROM jaegers WHERE kaijuKill = (SELECT MIN(kaijuKill) FROM jaegers);
-SELECT AVG(weight) FROM jaegers;
-UPDATE jaegers SET kaijuKill = kaijuKill + 1 WHERE status = 'true';
-DELETE FROM jaegers WHERE status = 'false';
+SELECT *
+FROM jaegers;
+
+SELECT *
+FROM jaegers
+WHERE status = 'true';
+
+SELECT *
+FROM jaegers
+WHERE mark = 'Mark-1'
+OR mark = 'Mark-4';
+
+SELECT *
+FROM jaegers
+ORDER BY mark
+DESC;
+
+SELECT *
+FROM jaegers
+WHERE launch = (SELECT MIN(launch) FROM jaegers)
+LIMIT 1;
+
+SELECT *
+FROM jaegers 
+WHERE kaijuKill = (SELECT MAX(kaijuKill) FROM jaegers);
+
+SELECT *
+FROM jaegers
+WHERE kaijuKill = (SELECT MIN(kaijuKill) FROM jaegers);
+
+SELECT AVG(weight)
+FROM jaegers;
+
+UPDATE jaegers
+SET kaijuKill = kaijuKill + 1
+WHERE status = 'true';
+
+DELETE FROM jaegers
+WHERE status = 'false';
